f96089184b6987cffad8748fed80cf8b387e3d94
Ruby <necrelox@proton.me>
2024-02-10 18:53:54 +0100

    fix: add cookie secret

--- Source/HTTP/Plugin/CookiePlugin.ts
+++ Source/HTTP/Plugin/CookiePlugin.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import cookie from '@fastify/cookie';
+import { randomUUID } from 'crypto';
 
 import { IPlugin } from '@/HTTP/Interface';
 
 export class CookiePlugin implements IPlugin {
     configure(app: FastifyInstance): void {
-        app.register(cookie);
+        app.register(cookie, {
+            secret: randomUUID(),
+        });
     }
 }
